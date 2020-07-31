04ef6288191c8c0dec3a458493ae7c83bdaccfb6
eliorivero <eliorivero@users.noreply.github.com>
2020-07-31 03:48:19 +0000

    Update from master

---
+++ _inc/blocks/story/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd76e5cc93dc5881c33eb7bce430ec6a5');
